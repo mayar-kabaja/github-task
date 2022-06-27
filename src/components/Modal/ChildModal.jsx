bd8d158f986a4bb0625a6fa13af9f1b18729051e
mayar-kabaja <kabajamayar@gmail.com>
2022-06-27 12:32:53 +0300

    fix Modal Relates #13

--- src/components/Modal/ChildModal.jsx
+++ src/components/Modal/ChildModal.jsx
@@ -4,13 +4,16 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { handleCloseChildModal } from '../Features/PullRequests';
+import { CircularProgress } from '@mui/material';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
+  overflow:'scroll',
   transform: 'translate(-50%, -50%)',
   width: 400,
+  height : 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -21,7 +24,7 @@ const style = {
 
 export default function ChildModal() {
   const dispatch = useDispatch();
-  const {openChildModal , pulls} = useSelector((store) => store.pulls)
+  const {openChildModal , pulls , isLoading} = useSelector((store) => store.pulls)
   console.log(pulls);
   const handleClose = () => {
     dispatch(handleCloseChildModal());
@@ -36,11 +39,12 @@ export default function ChildModal() {
         aria-labelledby="child-modal-title"
         aria-describedby="child-modal-description"
       >
-        <Box sx={{ ...style, width: 200 }}>
+        <Box sx={{ ...style, width: 200 , overflow:'scroll'}}>
           <h2 id="child-modal-title">Pulls</h2>
-          {
-            openChildModal&& pulls.length ? pulls.map(({title})=><p key={title}>{title}</p>
-            ): null
+          { !isLoading ?
+             (pulls.length ? pulls.map(({title})=><p key={title}>{title}</p>
+            ): <h3>No Pulls</h3>)
+            : <CircularProgress/>
           }
           <Button onClick={handleClose}>Close</Button>
         </Box>
