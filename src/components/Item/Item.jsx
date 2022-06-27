51e5b5780cd23f8222c4358a587f7b0344d08e58
mayar-kabaja <kabajamayar@gmail.com>
2022-06-27 10:26:18 +0300

    Get Repos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,8 +1,16 @@
 import { ListItem, ListItemAvatar, Avatar,ListItemText } from '@mui/material';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { getRepos, handleOpen } from '../Features/Repos';
+
 function Item({login , avatar}) {
+  const dispatch = useDispatch();
+  const handleClick = ()=>{
+    dispatch(getRepos(login));
+    dispatch(handleOpen());
+  }
   return (
-    <ListItem alignItems="flex-start" style={{ cursor: 'pointer'}}>
+    <ListItem alignItems="flex-start" style={{ cursor: 'pointer'}} onClick={handleClick}>
         <ListItemAvatar>
           <Avatar alt="Remy Sharp" src={avatar} />
         </ListItemAvatar>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { Users } from "./components";
+import { reposSlice, Users } from "./components";
 
 export const store = configureStore({
     reducer :{
-        Users
+        Users,
+        repose : reposSlice.reducer
     }
 })
